3a9377fc3383b88bd1a9d33b824763ea47ab80fd
Christopher Aiken <christopher.aiken@btinternet.com>
2016-11-30 10:00:49 +0000

    Create Earlyindex.php

---
+++ app/Earlyindex.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en"> 
+	<head>
+		<meta charset="utf-8"/>
+		<title>Portlethen site</title>
+		<link rel="stylesheet" type="text/css" href="css/indexold.css">
+	</head>
+
+	<body>
+		<!--Logo-->
+		<div id="Logo">
+        <img src="images/Chris.jpg" alt="chris.jpg" height="150" width="250">
+		</div>
+
+		<!--Page Name-->
+		<div id="Pagename">
+		<p>Portlethen website Work HUB</p>
+		</div>
+
+		<!--Homepage Button -->
+		<div id="Infobutton">
+			<p>Pages</p>
+        <form>
+		<input class="button" onclick="window.location.href='index.php'" type="button" value="Homepage" />
+        </form>
+	<form>
+		<input class="button" onclick="window.location.href='map.php'" type="button" value="Map" />
+        </form>
+        <form>
+		<input class="button" onclick="window.location.href='Health.php'" type="button" value="Health" />
+        </form>
+        <form>
+		<input class="button" onclick="window.location.href='Clubs.php'" type="button" value="Clubs" />
+        </form>
+		</div>
+
+		<!--External link test -->
+		<div id="Locationbutton">
+        <form>
+		<input class="button" onclick="window.location.href='http://animasave.altervista.org/PHP%20Folder/PHP%20Index%20Page.php'" type="button" value="External link test" /> 
+		</form>
+		</div>
+
+		<!--Body Text-->
+		<div id="BodyText">
+		<p>
+		Test site page for us to experiment with.<br>
+		Please edit at will.</p>
+			<a class="twitter-timeline" href="https://twitter.com/TwitterDev">Tweets by TwitterDev</a> 
+			<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+		</div>
+
+		<div id="footer">
+		<address>Made 28 November 2016<br>
+  		Website by Blackrabbit - Unicorns -.</address>
+		</div>
+	</body>
+</html>
+Contact GitHub API Training Shop Blog About
+© 2016 GitHub, Inc. Terms Privacy Security Status Help

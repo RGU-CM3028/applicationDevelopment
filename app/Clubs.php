0e9cdd116ef95431cef57d5405f0314579319a49
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-01 18:29:16 +0000

    Update Clubs.php

--- app/Clubs.php
+++ app/Clubs.php
@@ -22,23 +22,10 @@
 			<p>Pages</p>
         <form>
 		<input class="button" onclick="window.location.href='index.php'" type="button" value="Homepage" />
-        </form>
-	<form>
 		<input class="button" onclick="window.location.href='map.php'" type="button" value="Map" />
-        </form>
-        <form>
 		<input class="button" onclick="window.location.href='Health.php'" type="button" value="Health" />
-        </form>
-        <form>
 		<input class="button" onclick="window.location.href='Clubs.php'" type="button" value="Clubs" />
         </form>
-		</div>
-
-		<!--External link test -->
-		<div id="Locationbutton">
-        <form>
-		<input class="button" onclick="window.location.href='http://animasave.altervista.org/PHP%20Folder/PHP%20Index%20Page.php'" type="button" value="External link test" /> 
-		</form>
 		</div>
 
 		<!--Body Text-->

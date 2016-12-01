ec54371c97cd9a6200001e4b66ec95775b44220b
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-01 18:29:42 +0000

    Update index.php

--- app/index.php
+++ app/index.php
@@ -16,29 +16,15 @@
 		<div id="Pagename">
 		<p>Portlethen website - Homepage wip</p>
 		</div>
-
 		<!--Homepage Button -->
 		<div id="Infobutton">
 			<p>Pages</p>
         <form>
 		<input class="button" onclick="window.location.href='index.php'" type="button" value="Homepage" />
-        </form>
-	<form>
 		<input class="button" onclick="window.location.href='map.php'" type="button" value="Map" />
-        </form>
-        <form>
 		<input class="button" onclick="window.location.href='Health.php'" type="button" value="Health" />
-        </form>
-        <form>
 		<input class="button" onclick="window.location.href='Clubs.php'" type="button" value="Clubs" />
         </form>
-		</div>
-
-		<!--External link test -->
-		<div id="Locationbutton">
-        <form>
-		<input class="button" onclick="window.location.href='http://animasave.altervista.org/PHP%20Folder/PHP%20Index%20Page.php'" type="button" value="External link test" /> 
-		</form>
 		</div>
 
 		<!--Body Text-->

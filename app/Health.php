b168bec79ace4f021a164ee50a7fcd6ef5f2baaf
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-01 18:53:39 +0000

    Update Health.php

--- app/Health.php
+++ app/Health.php
@@ -30,7 +30,7 @@
 
 		<!--Body Text-->
 		<div id="BodyText">
-			<a class="twitter-timeline" href="https://twitter.com/TwitterDev" data-width="200"  data-height="200"> Tweets by @goodhealth</a><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+			<a class="twitter-timeline" href="https://twitter.com/goodhealth" data-width="200"  data-height="200"> Tweets by @goodhealth</a><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
     			<?php
         		include("dbconnect.php");
         		echo "Portlethean Health News / n Eat your 5 a day to stay healthy.";	

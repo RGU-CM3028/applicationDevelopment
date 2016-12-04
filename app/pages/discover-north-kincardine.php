1f77d56ae5ff4e52085a23f8f63cc9be14c84a79
Roulette Rogue <rrogue@users.noreply.github.com>
2016-12-04 20:38:00 +0000

    pages added

---
+++ app/pages/discover-north-kincardine.php
@@ -0,0 +1,14 @@
+<?php
+    // imports the header/navigation
+    include(../inc/header.inc);
+?>
+
+    <section>
+        <!-- CONTENT GOES IN HERE -->
+
+    </section>
+
+<?php
+    // imports the footer
+    include(../inc/footer.inc);
+?>

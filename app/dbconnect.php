0b1b6a1558733a0967a856ed2282999e14e818e0
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-05 20:13:11 +0000

    Tidy up and added comments to assist
    Tidy up and added comments to assist us if we need it :)

--- app/dbconnect.php
+++ app/dbconnect.php
@@ -1,19 +1,20 @@
 <?php
-
+//Code for connecting to a database.
 define('DB_SERVER', 'eu-cdbr-azure-north-e.cloudapp.net');
 define('DB_USERNAME', 'b01be6ac34b5a8');
 define('DB_PASSWORD', '29762594');
 define('DB_DATABASE', 'bruwebproject');
 
+//This puts the database into a variable called $db.
 $db = new mysqli(DB_SERVER,
     DB_USERNAME, DB_PASSWORD,
     DB_DATABASE);
-
     if (!$db->ping()) {
         echo 'Lost connection, exiting after query #1';
     }
 
-    if($db->connect_errno){
-        echo("connection failed");
-        die('Connectfailed['.$db->connect_error.']');
+//This checks to see if the database connection is successful or not.
+if($db->connect_errno){
+    echo("connection failed");
+    die('Connectfailed['.$db->connect_error.']');
     }

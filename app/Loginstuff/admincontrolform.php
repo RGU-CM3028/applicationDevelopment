6578782dbb6b29ac39e4f8e2f0321632943c2f52
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-05 23:31:27 +0000

    Update admincontrolform.php

--- app/Loginstuff/admincontrolform.php
+++ app/Loginstuff/admincontrolform.php
@@ -9,45 +9,34 @@
 <?
 //This starts the sessions. And connects the database here.
 session_start();
-include ("dbconnect.php");    
-        
-//This checks to see if the user is logged in or not. If the user is logged in then the user is able to see the following text.
-if (isset($_SESSION['username']))
-{
-    echo "<p>Hello " . $_SESSION['username'] . "</p>";
-    $sql = "SELECT * FROM users WHERE username='". $_SESSION['username'] . "'";
-    $result = $db->query($sql);
-    while($row = $result->fetch_array())
-    {
+include ("dbconnect.php");
+//This prepares the accesslevel.
+$accesslevel = $_SESSION['userType'];
+    
+//This displays the function contents	
+displayAccessLevelInformation($accesslevel);
+//This is the function doing its magic on a set piece of code.	
+function displayAccessLevelInformation($accessLevel){
+	if ($accessLevel == "admin") {
+	    ?>
+	    <!--Admin control pannel-->    
+	    <form method="post" action="admincontrol.php">
+        <p><input type="text" name="placeholder" value="" placeholder="placeholder"></p>
+        <p><input type="password" name="placeholder" value="" placeholder="placeholder"></p>
+        <p class="submit"><input type="submit" name="commit" value="Placeholder"></p>
+        </form>
+        <a href="index.php">Return to login screen</a>
+	    <?
         echo "<p>User type is " . $_SESSION['userType'] . "</p>";
         echo "<p>Welcome to the admin control pannel.</p>";
+	} else {
+            echo "<p>No access allowed. Please head to another page.<p>
+            ?>
+            <a href="index.php">Return to login screen</a>
+            <?
     }
-    ?>
-    
-     <!--This is a link to go back to the index page-->
-    <a href="lindex.php">Index page</a>
-    <!--This is a link to logout the site-->
-    <a href="logout.php">Logout</a>
-    
-    <?
 }
-    //If the user isnt logged in then they see the following form. To log in.
-    else
-{
 ?>
-    
-    <!--This is the form header-->
-    <h1>Admin Control</h1> 
-    
-    <?   
-    <!--This is placeholder till I finish the security on the main login stuff-->
-    <form method="post" action="admincontrol.php">
-    <p><input type="text" name="placeholder" value="" placeholder="Username please"></p>
-    <p><input type="password" name="placeholder" value="" placeholder="Password please"></p>
-    <p class="submit"><input type="submit" name="commit" value="Login"></p>
-    </form>
-
-<? } ?>
 
 </body>
 </html>

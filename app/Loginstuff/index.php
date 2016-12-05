2c88b96decae97296108904a5021f748f13a4651
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-05 16:43:32 +0000

    Update index.php

--- app/Loginstuff/index.php
+++ app/Loginstuff/index.php
@@ -21,25 +21,32 @@ if (isset($_SESSION['username']))
         echo "<p>User type is " . $_SESSION['userType'] . "</p>";
     }
     ?>
+    
     <!--This is a link to logout the site-->
     <a href="logout.php">Logout</a>
     
     <?
 } else {
 ?>
-    <!--This is the form used to login-->
+    
+    <!--This is the form header-->
     <h1>Login</h1> 
+    
     <?
+    //This is the error message for if there is a login fail.
     if(isset($_GET["Loginfail"]))  
     {  
         echo "<p><font color='red'>Please make sure you enter the correct information.</font></p>";  
  }  
     ?>
+    
+    <!--This is the form used to login-->
     <form method="post" action="checklogin.php">
     <p><input type="text" name="username" value="" placeholder="Username please"></p>
     <p><input type="password" name="password" value="" placeholder="Password please"></p>
     <p class="submit"><input type="submit" name="commit" value="Login"></p>
     </form>
+    
     <!--This is a link to signup to the site-->
     <a href="signupform.php">Signup</a>
 

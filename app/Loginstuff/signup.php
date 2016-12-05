22a1ef3ec27b640d30e38958854644ea7938d9b7
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-05 15:31:00 +0000

    Update signup.php

--- app/Loginstuff/signup.php
+++ app/Loginstuff/signup.php
@@ -1,9 +1,18 @@
 <?php
 include ("db_connect.php");
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 //This is the fields from the signup form
 $myusername = $_POST["username"];
 $mypassword = $_POST["password"];
 $passwordcheck = $_POST["passwordcheck"];
+$myusername = stripslashes($myusername);
+$myusername = mysqli_real_escape_string($db, $myusername);
+$mypassword = stripslashes($mypassword);
+$mypassword = mysqli_real_escape_string($db, $mypassword);
+$passwordcheck = stripslashes($passwordcheck);
+$passwordcheck = mysqli_real_escape_string($db, $passwordcheck);
 //This checks if there is any spaces in the user entered data
 if (strpos($myusername, ' ') !== false) {
     $userspace = 'true';
@@ -16,31 +25,25 @@ if (strpos($passwordcheck, ' ') !== false) {
 }
 //This tests to see if there is any spaces in the text
 if($userspace=='true' || $passspace=='true' || $pass2space=='true') {
-    header("location:signupform.php?space=1");
+    //header("location:signupform.php?space=1");
     die();
 }
 //This checks to see if the fields are empty or not.
 if(empty($myusername) || empty($mypassword) || empty($passwordcheck))
     {
-    header("location:signupform.php?empty=1");
+    //header("location:signupform.php?empty=1");
     die();
 }
 //This checks to see if the username is taken or not.
-$dup = mysql_query("SELECT username FROM users WHERE username='$myusername'");
-$userchecker = mysql_fetch_assoc($dup);
-if(mysql_num_rows($dup) >0){
-    header("location:signupform.php?dup=1");
+$dup = mysqli_query("SELECT username FROM users WHERE username='$myusername'");
+$userchecker = mysqli_fetch_assoc($dup);
+if(mysqli_num_rows($dup) >0){
+    //header("location:signupform.php?dup=1");
     die();
 }
 //This checks if the password is 100% what the user typed
 if($mypassword==$passwordcheck)
 {
-    $myusername = stripslashes($myusername);
-    $myusername = mysql_real_escape_string($myusername);
-    $mypassword = stripslashes($mypassword);
-    $mypassword = mysql_real_escape_string($mypassword);
-    $passwordcheck = stripslashes($passwordcheck);
-    $passwordcheck = mysql_real_escape_string($passwordcheck);
     $sql = "INSERT INTO users (username, password, userType) VALUES ('". $myusername ."', '" .$mypassword."', 'reader')";
     if (mysqli_query($db, $sql)) {
     } else {
@@ -48,10 +51,11 @@ if($mypassword==$passwordcheck)
     }
     session_start();
     $_SESSION['username'] = $myusername;
-    header("location:index.php");
+    $_SESSION['userType'] = 'reader';
+    //header("location:index.php");
     $sql = "INSERT INTO users (username, password, userType) VALUES ('". $myusername ."', '" .$mypassword."', 'reader')";
 } else {
-    header("location:signupform.php?same=1");
+    //header("location:signupform.php?same=1");
     die();
 }
 ?>

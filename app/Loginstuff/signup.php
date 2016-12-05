71118c948c04cc32537e78804a0ea454240e4f6d
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-05 15:35:36 +0000

    Update signup.php

--- app/Loginstuff/signup.php
+++ app/Loginstuff/signup.php
@@ -1,8 +1,6 @@
 <?php
 include ("dbconnect.php");
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+
 //This is the fields from the signup form
 $myusername = $_POST["username"];
 $mypassword = $_POST["password"];
@@ -25,20 +23,20 @@ if (strpos($passwordcheck, ' ') !== false) {
 }
 //This tests to see if there is any spaces in the text
 if($userspace=='true' || $passspace=='true' || $pass2space=='true') {
-    //header("location:signupform.php?space=1");
+    header("location:signupform.php?space=1");
     die();
 }
 //This checks to see if the fields are empty or not.
 if(empty($myusername) || empty($mypassword) || empty($passwordcheck))
     {
-    //header("location:signupform.php?empty=1");
+    header("location:signupform.php?empty=1");
     die();
 }
 //This checks to see if the username is taken or not.
 $dup = mysqli_query("SELECT username FROM users WHERE username='$myusername'");
 $userchecker = mysqli_fetch_assoc($dup);
 if(mysqli_num_rows($dup) >0){
-    //header("location:signupform.php?dup=1");
+    header("location:signupform.php?dup=1");
     die();
 }
 //This checks if the password is 100% what the user typed
@@ -52,10 +50,10 @@ if($mypassword==$passwordcheck)
     session_start();
     $_SESSION['username'] = $myusername;
     $_SESSION['userType'] = 'reader';
-    //header("location:index.php");
+    header("location:index.php");
     $sql = "INSERT INTO users (username, password, userType) VALUES ('". $myusername ."', '" .$mypassword."', 'reader')";
 } else {
-    //header("location:signupform.php?same=1");
+    header("location:signupform.php?same=1");
     die();
 }
 ?>

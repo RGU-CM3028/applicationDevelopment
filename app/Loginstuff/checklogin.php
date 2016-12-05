5479371a2819eb01c832281f6eea6ba59c806035
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-05 20:08:18 +0000

    Update checklogin.php

--- app/Loginstuff/checklogin.php
+++ app/Loginstuff/checklogin.php
@@ -1,17 +1,18 @@
 <?php
+//Code that connects the database here.
 include ("dbconnect.php");
 
 //information from the index form.
 $myusername = $_POST["username"];
 $mypassword = $_POST["password"];
 
-//Security checks version1
+//Security checks Version1
 $myusername = stripslashes($myusername);
 $myusername = mysqli_real_escape_string($db,$myusername);
 $mypassword = stripslashes($mypassword);
 $mypassword = mysqli_real_escape_string($db,$mypassword);
 
-//checking to see if any usernames and password pairs match any in the database.
+//Code that checks to see if any usernames and password pairs match any in the database.
 $sql = "SELECT * FROM users WHERE username ='". $myusername ."' and password ='". $mypassword . "'";
 $result = $db->query($sql);
 $checker = 0;
@@ -19,7 +20,7 @@ while($row = $result->fetch_array()) {
     $checker = 1;
 }
 
-//Code for getting usertype extracted
+//Code for getting usertype extracted for the session.
 $userType = "";
 $boom = "SELECT userType FROM users WHERE username ='". $myusername ."' and password ='". $mypassword . "'";
 $result = $db->query($boom);
@@ -27,7 +28,8 @@ while($row = $result->fetch_array()){
 $userType = $row['userType'];
 }   
 
-//This checks if any pairs matched or not. And send the user back to the index page.
+//This checks if any pairs matched or not. And send the user back to the index page. 
+//If the user managed to log in the username and usertype is saved as a session.
 if($checker==1){
     session_start();
     $_SESSION['username'] = $myusername;

afc4b4534d337c9d79b5d3ec8eb1ca716215275b
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-05 15:28:44 +0000

    Update checklogin.php

--- app/Loginstuff/checklogin.php
+++ app/Loginstuff/checklogin.php
@@ -1,20 +1,16 @@
 <?php
 include ("db_connect.php");
 //information from the index form
+
 $myusername = $_POST["username"];
 $mypassword = $_POST["password"];
 
-//Saves the username as it
-$susername = $myusername;
-
 //checking to see if any usernames and password pairs match any in the database
 $myusername = stripslashes($myusername);
-$myusername = mysql_real_escape_string($myusername);
+$myusername = mysqli_real_escape_string($db,$myusername);
 $mypassword = stripslashes($mypassword);
-$mypassword = mysql_real_escape_string($mypassword);
-echo "$myusername";
-echo "$mypassword";
-die();
+$mypassword = mysqli_real_escape_string($db,$mypassword);
+
 $sql = "SELECT * FROM users WHERE username ='". $myusername ."' and password ='". $mypassword . "'";
 //The following code checks to see if any match
 $result = $db->query($sql);
@@ -22,11 +18,19 @@ $checker = 0;
 while($row = $result->fetch_array()) {
     $checker = 1;
 }
-      
+
+//Test code for getting usertype extracted
+$boom = "SELECT usertype FROM users WHERE username ='". $myusername ."' and password ='". $mypassword . "'";
+$result = $db->query($boom);
+while($row = $result->fetch_array()){
+$userType = $row['userType'];
+echo $userType;    
+    
 //This deals with if any matched or not. And send the user back to the index page
 if($checker==1){
     session_start();
-    $_SESSION['username'] = $susername;
+    $_SESSION['username'] = $myusername;
+    $_SESSION['userType'] = $userType;
     header("location:index.php");
 } else {
     header("location:index.php?Loginfail=1");

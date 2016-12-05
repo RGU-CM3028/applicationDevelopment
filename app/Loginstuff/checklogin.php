790b9b209536d6c992be38ef50c96eeef9bc717a
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-05 10:13:34 +0000

    Update checklogin.php

--- app/Loginstuff/checklogin.php
+++ app/Loginstuff/checklogin.php
@@ -3,11 +3,7 @@ include ("db_connect.php");
 //information from the index form
 $myusername = $_POST["username"];
 $mypassword = $_POST["password"];
-//Need to possibly get this working for security
-//$myusername = mysqli_real_escape_string($myusername);
-//$mypassword = mysqli_real_escape_string($mypassword);
-//$myusername = stripcslashes($myusername);
-//$mypassword = stripcslashes($mypassword);
+
 //checking to see if any usernames and password pairs match any in the database
 $myusername = stripslashes($myusername);
 $myusername = mysql_real_escape_string($myusername);
@@ -20,8 +16,7 @@ $checker = 0;
 while($row = $result->fetch_array()) {
     $checker = 1;
 }
-    
-    
+      
 //This deals with if any matched or not. And send the user back to the index page
 if($checker==1){
     session_start();

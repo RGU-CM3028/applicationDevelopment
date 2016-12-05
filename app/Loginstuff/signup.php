1ce0e54443cfac448d7eadc25614cdc86b237304
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-05 12:04:08 +0000

    Update signup.php

--- app/Loginstuff/signup.php
+++ app/Loginstuff/signup.php
@@ -16,39 +16,20 @@ if (strpos($passwordcheck, ' ') !== false) {
 }
 //This tests to see if there is any spaces in the text
 if($userspace=='true' || $passspace=='true' || $pass2space=='true') {
-    session_start();
-    $_SESSION['Signupfail'] = "Fail1";
-    header("location:signupform.php");
+    header("location:signupform.php?space=1");
     die();
 }
 //This checks to see if the fields are empty or not.
 if(empty($myusername) || empty($mypassword) || empty($passwordcheck))
     {
-    session_start();
-    $_SESSION['Signupfail'] = "Fail2";
-    header("location:signupform.php");
+    header("location:signupform.php?empty=1");
     die();
 }
 //This checks to see if the username is taken or not.
 $dup = mysql_query("SELECT username FROM users WHERE username='$myusername'");
 $userchecker = mysql_fetch_assoc($dup);
 if(mysql_num_rows($dup) >0){
-    session_start();
-    $_SESSION['Signupfail'] = "Fail4";
-    header("location:signupform.php");
-    die();
-}
-$myusername = mysql_real_escape_sequence($_GET['username']);
-$query = "SELECT username FROM users WHERE username = '$myusername'";
-if ($myusername == $userchecker['username']) {
-    echo "Not avaifddd";
-    die();
-}
-$results = @mysql_query($query);
-if(mysql_num_rows($results) > 0)
-{
-  // Username exists.
-  echo "Error: Username already taken.";
+    header("location:signupform.php?dup=1");
     die();
 }
 //This checks if the password is 100% what the user typed
@@ -70,9 +51,7 @@ if($mypassword==$passwordcheck)
     header("location:index.php");
     $sql = "INSERT INTO users (username, password, userType) VALUES ('". $myusername ."', '" .$mypassword."', 'reader')";
 } else {
-    session_start();
-    $_SESSION['Signupfail'] = "Fail3";
-    header("location:signupform.php");
+    header("location:signupform.php?same=1");
     die();
 }
 ?>

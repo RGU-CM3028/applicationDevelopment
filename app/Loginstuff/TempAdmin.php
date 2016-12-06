e0c03c94af72d85f845d52335d4149d5c73005c0
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-06 00:56:32 +0000

    Update TempAdmin.php

--- app/Loginstuff/TempAdmin.php
+++ app/Loginstuff/TempAdmin.php
@@ -45,7 +45,7 @@ if($userspace=='true' || $passspace=='true' || $pass2space=='true') {
 //This checks to see if the fields are empty or not.
 if(empty($myusername) || empty($mypassword) || empty($passwordcheck))
     {
-    header("location:signupform.php?empty=1");
+    header("location:Tempadminform.php?empty=1");
     die();
 }
 
@@ -53,7 +53,7 @@ if(empty($myusername) || empty($mypassword) || empty($passwordcheck))
 $dup = mysqli_query($db, "SELECT username FROM users WHERE username='$myusername'");
 $userchecker = mysqli_fetch_assoc($dup);
 if(mysqli_num_rows($dup) >0){
-    header("location:signupform.php?dup=1");
+    header("location:Tempadminform.php?dup=1");
     die();
 } 
 
@@ -71,7 +71,7 @@ if($mypassword==$passwordcheck) {
     $sql = "INSERT INTO users (username, password, userType) VALUES ('". $myusername ."', '" .$mypassword."', 'admin')";
     
 } else {
-    header("location:signupform.php?same=1");
+    header("location:Tempadminform.php?same=1");
     die();
 }
 ?>

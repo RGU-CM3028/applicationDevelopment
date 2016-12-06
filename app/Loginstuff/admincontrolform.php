a8f4641181e45a184ba2d6907049f780e8e565ff
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-06 16:55:41 +0000

    Update admincontrolform.php

--- app/Loginstuff/admincontrolform.php
+++ app/Loginstuff/admincontrolform.php
@@ -9,15 +9,29 @@ $accesslevel = $_SESSION['userType'];
 displayAccessLevelInformation($accesslevel);
 //This is the function doing its magic on a set piece of code.	
 function displayAccessLevelInformation($accessLevel){
+	
+	//If the user is an admin, the following code works
 	if ($accessLevel == "admin") {
 		?>
-		<!--This is where the controls would go-->    
-		<a href="admincontrolform.php">Admin Control Pannel</a>    
+		<!--This is the control panel instructions(version1)-->    
+		<p>This is where the admin would select a user from the dropbox of all users, then selects who they want to make an admin.</p> 
+		<p>This is also where the admin can delete users.</p>
+		<p>This is where the admin .</p>
+
+		<!--This is where the controls would go--> 
+		<h1>Admin controls</h1>
+    		<form method="post" action="admincontrols.php">
+    			<p><input type="text" name="username" value="" placeholder="Username please"></p>
+    			<p><input type="password" name="password" value="" placeholder="Placeholder please"></p>
+    			<p class="submit"><input type="submit" name="commit" value="Login"></p>
+    		</form>
+
+            	<!-- This is used to return to the login screen-->
+            	<a href="index.php">Return to login screen</a>
 		<?
 	}
-	else 
-	{
-		//This sends non admins back to the index page
+	//Otherwise the code sends the user back to the index page.
+	else {
 		header("location:index.php");
 		die();
 	}

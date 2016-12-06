77ceb25ec6cae4a8ba91caceb60358b0b11c4d95
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-06 00:25:49 +0000

    Update signup.php

--- app/Loginstuff/signup.php
+++ app/Loginstuff/signup.php
@@ -2,6 +2,10 @@
 //This connects the database here.
 include ("dbconnect.php");
 
+-ini_set('display_errors', 1);
+ -ini_set('display_startup_errors', 1);
+ -error_reporting(E_ALL);
+
 //This is the fields from the signup form.
 $myusername = $_POST["username"];
 $mypassword = $_POST["password"];
@@ -10,10 +14,10 @@ $passwordcheck = $_POST["passwordcheck"];
 //Security checking V1.
 $myusername = stripslashes($myusername);
 $myusername = mysqli_real_escape_string($db, $myusername);
-//$mypassword = stripslashes($mypassword);
-//$mypassword = mysqli_real_escape_string($db, $mypassword);
-//$passwordcheck = stripslashes($passwordcheck);
-//$passwordcheck = mysqli_real_escape_string($db, $passwordcheck);
+$mypassword = stripslashes($mypassword);
+$mypassword = mysqli_real_escape_string($db, $mypassword);
+$passwordcheck = stripslashes($passwordcheck);
+$passwordcheck = mysqli_real_escape_string($db, $passwordcheck);
 $salt = "qwertgfdert45t456545655";
 $mypassword = $mypassword.$salt;
 $mypassword = sha256($mypassword);
@@ -38,14 +42,14 @@ if (strpos($passwordcheck, ' ') !== false) {
 
 //This tests to see if there is any spaces in the text
 if($userspace=='true' || $passspace=='true' || $pass2space=='true') {
-    header("location:signupform.php?space=1");
+    //header("location:signupform.php?space=1");
     die();
 }
 
 //This checks to see if the fields are empty or not.
 if(empty($myusername) || empty($mypassword) || empty($passwordcheck))
     {
-    header("location:signupform.php?empty=1");
+    //header("location:signupform.php?empty=1");
     die();
 }
 
@@ -53,7 +57,7 @@ if(empty($myusername) || empty($mypassword) || empty($passwordcheck))
 $dup = mysqli_query($db, "SELECT username FROM users WHERE username='$myusername'");
 $userchecker = mysqli_fetch_assoc($dup);
 if(mysqli_num_rows($dup) >0){
-    header("location:signupform.php?dup=1");
+    //header("location:signupform.php?dup=1");
     die();
 } 
 
@@ -64,15 +68,14 @@ if($mypassword==$passwordcheck) {
     } else {
         echo "Error: " . $sql . "<br>" . mysqli_error($db);
     }
-    session_start();
-    $_SESSION['username'] = $myusername;
-    $_SESSION['userType'] = 'reader';
-    header("location:index.php");
-    
+    //session_start();
+    //$_SESSION['username'] = $myusername;
+    //$_SESSION['userType'] = 'reader';
+    //header("location:index.php");
     $sql = "INSERT INTO users (username, password, userType) VALUES ('". $myusername ."', '" .$mypassword."', 'reader')";
     
 } else {
-    header("location:signupform.php?same=1");
+    //header("location:signupform.php?same=1");
     die();
 }
 ?>

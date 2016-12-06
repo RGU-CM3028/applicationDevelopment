5606a8b38f9d9c152ee3b722f1eddaae7b581490
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-06 16:24:16 +0000

    Update admincontrolform.php

--- app/Loginstuff/admincontrolform.php
+++ app/Loginstuff/admincontrolform.php
@@ -1,42 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Admin Control Pannel</title>
-</head>
-<body>
-
-<?
-//This starts the sessions. And connects the database here.
+<?php
+//This starts the sessions. And connects the database here..
 session_start();
-include ("dbconnect.php");
+include ("dbconnect.php");    
 //This prepares the accesslevel.
 $accesslevel = $_SESSION['userType'];
-    
+
 //This displays the function contents	
 displayAccessLevelInformation($accesslevel);
 //This is the function doing its magic on a set piece of code.	
 function displayAccessLevelInformation($accessLevel){
 	if ($accessLevel == "admin") {
-	    ?>
-	    <!--Admin control pannel-->    
-	    <form method="post" action="admincontrol.php">
-        <p><input type="text" name="placeholder" value="" placeholder="placeholder"></p>
-        <p><input type="password" name="placeholder" value="" placeholder="placeholder"></p>
-        <p class="submit"><input type="submit" name="commit" value="Placeholder"></p>
-        </form>
-        <a href="index.php">Return to login screen</a>
-	    <?
-        echo "<p>User type is " . $_SESSION['userType'] . "</p>";
-        echo "<p>Welcome to the admin control pannel.</p>";
-	} else {
-            echo "<p>No access allowed. Please head to another page.<p>
-            ?>
-            <a href="index.php">Return to login screen</a>
-            <?
-    }
+		?>
+		<!--This is where the controls would go-->    
+		<a href="admincontrolform.php">Admin Control Pannel</a>    
+		<?
+	}
+	else 
+	{
+		echo "You cant enter here!";
+		?>
+		<!--This would lead you back to the main page-->    
+		<a href="index.php">Return to index page</a>    
+		<?
+	}
+		
 }
-?>
-
-</body>
-</html>

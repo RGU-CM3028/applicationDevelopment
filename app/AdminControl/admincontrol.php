2bbfb06134239ebc936393f58679af0e29b2b363
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-07 18:53:29 +0000

    Update admincontrol.php

--- app/AdminControl/admincontrol.php
+++ app/AdminControl/admincontrol.php
@@ -84,12 +84,20 @@ if ($adminusername == $_SESSION['username']){
 //needs rest of code
 if($adminchoice == "delete"){
 	$query = "DELETE FROM users WHERE username = '".$adminusername."' LIMIT1";
-	header("location:admincontrolform.php?delete=1");
-    	die();
+	if (mysqli_query($db, $query)) {    
+		header("location:admincontrolform.php?delete=1");
+    		die();
+    	} else {
+        	echo "Error: " . $query . "<br>" . mysqli_error($db);
+    	}
 } elseif($adminchoice == "usertype") {
 	$sql = "UPDATE users SET userType='".$adminuserchoice."' WHERE username='".$adminusername."'";
-	header("location:admincontrolform.php?update=1");
-    	die();
+	if (mysqli_query($db, $query)) {    
+		header("location:admincontrolform.php?update=1");
+    		die();
+    	} else {
+        	echo "Error: " . $query . "<br>" . mysqli_error($db);
+    	}
 } elseif($adminchoice == "") {
 	header("location:admincontrolform.php?select=1");
     	die();

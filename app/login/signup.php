e4405a179112ddfd5dd9e713e9eeb4dc8ea75432
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-07 11:23:01 +0000

    Update signup.php

--- app/login/signup.php
+++ app/login/signup.php
@@ -1,6 +1,5 @@
 <?php
 //This connects the database here.
-//include ("dbconnect.php");
 include("../dbconnect.php");
 
 //html change safety check. This is to catch out any attempt to change variables and so on in the html.

0ad52efed627bf0dae4da87173cdf23c2b665d40
Alexa Noel <alexanowel@live.fr>
2016-12-07 21:37:49 +0000

    Refine home page

--- app/index.php
+++ app/index.php
@@ -1,23 +1,20 @@
 <?php
-    // imports the header/navigation
-    include("inc/header.inc");
     include("./dbconnect.php");
 ?>
-  <section>
-		<div id="BodyText">
-    			<?php
-        			echo "Welcome to GoPortlethean. We are an collection of local and progressive sports clubs who are working together to develop safe and fun sport & fitness activities in the Portlethen area. We work with sportscotland and other organisations to develop our clubs. Our website is a single access point to find out more about the fantastic sporting opportunities in our area.";
-     			?>
-			<br>
-			<img src="images/IndexImage1.png" alt="Indeximage1.png" height="150" width="150">
-			<img src="images/IndexImage2.png" alt="Indeximage2.png" height="150" width="150">
-			<img src="images/IndexImage3.png" alt="Indeximage3.png" height="150" width="150">
 
+<link rel="stylesheet" type="text/css" href="css/main.css">
 
-		</div>
-</section>
+<header id="headerMenu" class="clearfix">
+    <ul id="homeMenu">
+      <li class="navigation"><a href="./pages/clubs/">Clubs</a></li>
+      <li class="navigation"><a href="./pages/health/">Health and Wellbeing</a></li>
+      <li class="navigation"><a href="./pages/map/">Discover North Kincardineshire</a></li>
+    </ul>
+  </div>
+</header>
 
-<?php
-    // imports the footer
-    include("inc/footer.inc");
-?>
+	<div id="homePage">
+    <h1 id="homeTitle">Welcome to Sportlethen</h1>
+    <h2 id="homeSubTitle">Be healthy. Feel athletic.</h2>
+		<img src="images/logo.png" />
+	</div>

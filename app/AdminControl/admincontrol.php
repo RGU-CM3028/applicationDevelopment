752a6df71f096da596eeefced9b8aef887753287
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-07 12:09:59 +0000

    Update admincontrol.php

--- app/AdminControl/admincontrol.php
+++ app/AdminControl/admincontrol.php
@@ -52,14 +52,10 @@ $adminusername = stripslashes($adminusername);
 $adminusername = mysqli_real_escape_string($db,$adminusername);
 
 //HTML change safety check
-if($adminchoice != "delete" || $adminchoice != "usertype" || $adminchoice != ""){
-  echo "Oh no you dont get to change the html on us";
-  die();
-}
-if($adminuserchoice != "reader" || $adminuserchoice != "admin" || $adminuserchoice != "unspecified" || $adminuserchoice != ""){
-  echo "Oh no you dont get to change the html on us";
-  die();
-}
+//if($adminchoice != "delete" || $adminchoice != "usertype" || $adminchoice != ""){
+  //echo "Oh no you dont get to change the html on us";
+  //die();
+//}
 
 //needs rest of code
 if($adminchoice == "delete"){

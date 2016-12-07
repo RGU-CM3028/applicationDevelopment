ac00448e1376dc31898e64c8ab9e854a671d31e1
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-07 20:56:39 +0000

    Update TempAdmin.php

--- app/Loginstuff/TempAdmin.php
+++ app/Loginstuff/TempAdmin.php
@@ -59,7 +59,7 @@ if(mysqli_num_rows($dup) >0){
 
 //This compares the passwords. If the match then the user is created. If not then the user is told to check again.
 if($mypassword==$passwordcheck) {
-    $sql = "INSERT INTO users (username, password, userType) VALUES ('". $myusername ."', '" .$mypassword."', 'admin')";
+    $sql = "INSERT INTO users (username, password, userType) VALUES ('". $myusername ."', '" .$mypassword."', 'siteadmin')";
     if (mysqli_query($db, $sql)) {        
     } else {
         echo "Error: " . $sql . "<br>" . mysqli_error($db);
@@ -68,7 +68,7 @@ if($mypassword==$passwordcheck) {
     $_SESSION['username'] = $myusername;
     $_SESSION['userType'] = 'admin';
     header("location:index.php");
-    $sql = "INSERT INTO users (username, password, userType) VALUES ('". $myusername ."', '" .$mypassword."', 'admin')";
+    $sql = "INSERT INTO users (username, password, userType) VALUES ('". $myusername ."', '" .$mypassword."', 'siteadmin')";
     
 } else {
     header("location:Tempadminform.php?same=1");

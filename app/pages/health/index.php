b1337454169d28671da3e1faf063585897e08c94
Alexa Noel <alexanowel@live.fr>
2016-12-07 20:20:17 +0000

    facebook news and health styling

--- app/pages/health/index.php
+++ app/pages/health/index.php
@@ -4,36 +4,57 @@ include("../../inc/header.inc");
 include('../../dbconnect.php');
 global $db;
 ?>
-	<section>
 	<!--Body Text-->
-		<div id="BodyText">
-			<a class="twitter-timeline" href="https://twitter.com/goodhealth" data-width="200"  data-height="200"> Tweets by @goodhealth</a><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
-
-			<?php
-				// if (!$db) {
-    		// 			die('Connect Error: ' . mysqli_connect_errno());
-				// }
-				$sql_query = "SELECT * FROM hwnews ORDER BY HWNewsDate DESC";
-				$result = $db->query($sql_query);
-				if($result->num_rows <= 0) {
-					echo "There's no news for the moment";
-				} else {
-					echo "<p><strong>All news: </strong>";
-					while($row = $result->fetch_array()){
-						echo "<div>
-								<h1> " . $row['HWNewsName'] . " </h1>
-										<p> " . $row['HWNewsDate'] . "</p>
-										<p> " . $row['HWNewsText'] . "</p>
-						</div>";
+		<div id="healthPage">
+			<div id="fb-root"></div>
+				<script>(function(d, s, id) {
+				  var js, fjs = d.getElementsByTagName(s)[0];
+				  if (d.getElementById(id)) return;
+				  js = d.createElement(s); js.id = id;
+				  js.src = "//connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v2.8&appId=1673121366290064";
+				  fjs.parentNode.insertBefore(js, fjs);
+				}(document, 'script', 'facebook-jssdk'));</script>
+			<h1 class="pageTitle"> Last news </h1>
+			<div id="healthContent">
+				<div>
+					<div class="fb-page"
+						data-href="https://www.facebook.com/Sportlethen/?fref=ts"
+						data-tabs="timeline"
+						data-weight="500"
+						data-height="800"
+						data-small-header="true"
+						data-adapt-container-width="false"
+						data-hide-cover="false"
+						data-show-facepile="true">
+						<blockquote
+							cite="https://www.facebook.com/Sportlethen/?fref=ts"
+							class="fb-xfbml-parse-ignore">
+							<a href="https://www.facebook.com/Sportlethen/?fref=ts">Sportlethen CSH</a>
+						</blockquote>
+					</div>
+				</div>
+				<?php
+					$sql_query = "SELECT * FROM hwnews ORDER BY HWNewsDate DESC";
+					$result = $db->query($sql_query);
+					if($result->num_rows <= 0) {
+						echo "There's no news for the moment";
+					} else {
+						while($row = $result->fetch_array()){
+							echo "<div id='news'>
+										<h1> " . $row['HWNewsName'] . " </h1>
+										<p id='newsDate'> " . $row['HWNewsDate'] . "</p>
+									<p id='newsText'> " . $row['HWNewsText'] . "</p>
+							</div>";
+						}
 					}
-				}
+					$result->close();
+	   			$db->close();
+	    	?>
 
-				$result->close();
-   				$db->close();
-    ?>
+				<a class="twitter-timeline" href="https://twitter.com/goodhealth" data-width="400"  data-height="800"> Tweets by @goodhealth</a>
+				<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+			</div>
 		</div>
-
-	</section>
 
 	<?php
 	// imports the footer

dbbe4253890c3f7815965bccfe90c6b74aeb8caf
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-07 15:11:49 +0000

    Update logout.php

--- app/login/logout.php
+++ app/login/logout.php
@@ -1,9 +1,7 @@
 <?
 //This code removes the users session. And also clears other sessions that are in the way.
 session_start();
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+
 //This clears the username session.
 if (isset($_SESSION['username']))
 {
@@ -17,5 +15,5 @@ if (isset($_SESSION['userType']))
 }
 
 //This sends the user back to the index page.
-//header("location:index.php");
+header("location:index.php");
 ?>

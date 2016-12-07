b6d475d960a2360d5bfc0dd75a15c33fdf2ceab8
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-07 11:50:50 +0000

    Update admincontrol.php

--- app/AdminControl/admincontrol.php
+++ app/AdminControl/admincontrol.php
@@ -18,64 +18,6 @@ function displayAccessLevelInformation($accesslevel){
 	}
 }
 
-//First half of checking for html code changes
-if(isset($_POST['choice'])) {
-    // it exists
-} else {
-    header("location:admincontrolform.php");
-    die();
-}
-if(isset($_POST['usertype'])) {
-    // it exists
-} else {
-    header("location:admincontrolform.php");
-    die();
-}
-if(isset($_POST['username'])) {
-    // it exists
-} else {
-    header("location:admincontrolform.php");
-    die();
-}
-
-//information from the index form.
-$adminchoice = $_POST["choice"];
-$adminuserchoice = $_POST["usertype"];
-$adminusername = $_POST["username"];
- 
-//Security checks Version1
-$adminchoice = stripslashes($adminchoice);
-$adminchoice = mysqli_real_escape_string($db,$adminchoice);
-$adminuserchoice = stripslashes($adminuserchoice);
-$adminuserchoice = mysqli_real_escape_string($db,$adminuserchoice);
-$adminusername = stripslashes($adminusername);
-$adminusername = mysqli_real_escape_string($db,$adminusername);
-
-//HTML change safety check
-if($adminchoice != "delete" || $adminchoice != "usertype" || $adminchoice != ""){
-  echo "Oh no you dont get to change the html on us";
-  die();
-}
-if($adminuserchoice != "reader" || $adminuserchoice != "admin" || $adminuserchoice != "unspecified" || $adminuserchoice != ""){
-  echo "Oh no you dont get to change the html on us";
-  die();
-}
-
-
-//Code that checks to see if any usernames and password pairs match any in the database.
-$sql = "SELECT * FROM users WHERE username ='". $adminusername . "' LIMIT 1;";
-$result = $db->query($sql);
-$checker = 0;
-while($row = $result->fetch_array()) {
-    $checker = 1;
-} 
-
-}
-if($checker != 1, $adminchoice != ""){
-  echo "Oh no you dont get to change the html on us";
-  die();
-}
-
 //needs rest of code
 if($adminchoice == "delete"){
   echo "delete oh noes";

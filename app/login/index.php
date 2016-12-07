e7822c9bc2a52b1f05bff74a5b143c4448a46b2c
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-07 15:10:18 +0000

    Update index.php

--- app/login/index.php
+++ app/login/index.php
@@ -7,6 +7,10 @@
 <body>
 
 <?
+	ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+	
 //This starts the sessions. And connects the database here..
 session_start();  
 include("../dbconnect.php");

011d356d438512ed2a3a57e83f14a852e92113c5
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-07 11:53:25 +0000

    Update admincontrol.php

--- app/AdminControl/admincontrol.php
+++ app/AdminControl/admincontrol.php
@@ -1,22 +1,19 @@
 <?php
 session_start();
 include("../dbconnect.php"); 
-	
-//This prepares the accesslevel.
-$accesslevel = $_SESSION['userType'];
-	
-//This displays the function contents	
-displayAccessLevelInformation($accesslevel);
-	
-//This is the function doing its magic on a set piece of code.	
-function displayAccessLevelInformation($accesslevel){
-	//This checks to see if the user is an admin or not. This protects the site from any html attack
-	if ($accesslevel != "admin") {
-		//This sends already signed in users back to the index page
-		header("location:../index.php");
-		die();
-	}
-}
+
+//information from the index form.
+$adminchoice = $_POST["choice"];
+$adminuserchoice = $_POST["usertype"];
+$adminusername = $_POST["username"];
+ 
+//Security checks Version1
+$adminchoice = stripslashes($adminchoice);
+$adminchoice = mysqli_real_escape_string($db,$adminchoice);
+$adminuserchoice = stripslashes($adminuserchoice);
+$adminuserchoice = mysqli_real_escape_string($db,$adminuserchoice);
+$adminusername = stripslashes($adminusername);
+$adminusername = mysqli_real_escape_string($db,$adminusername);
 
 //needs rest of code
 if($adminchoice == "delete"){
@@ -26,11 +23,9 @@ if($adminchoice == "delete"){
 } elseif($adminchoice == "") {
   echo "Nothing is there";
 }
-
 //reference
 //choice values = "", "delete", "usertype"
 //usertype values - "", "reader", "admin", "unspecified"
 //username values = "", $row['username']
-
 //$sql = "DELETE FROM MyGuests WHERE id=3";
 //$sql = "UPDATE MyGuests SET lastname='Doe' WHERE id=2";

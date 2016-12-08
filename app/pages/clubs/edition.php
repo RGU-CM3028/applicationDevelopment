c2b69de382511030ae25fb1d33332e3665db9f3c
florian peyron <flotos@hotmail.fr>
2016-12-08 13:10:25 +0000

    LiveDebugging - edition.php

--- app/pages/clubs/edition.php
+++ app/pages/clubs/edition.php
@@ -72,7 +72,7 @@ File handling the creation and edition of clubs
 			// }
 
 			//TODO get clubGenre and eventID
-			$sql_query = "INSERT INTO club (clubName, clubDescription, logoID, pname, adress, phone, email) VALUES('" . $_POST['clubName'] . "," . $_POST['clubDescription'] . "," . $mediaID . "," . $_POST['pname'] . "," .  $_POST['adress'] .  $_POST['phone'] .  $_POST['email'] . "'')";
+			$sql_query = "INSERT INTO club (clubName, clubDescription, logoID, pname, adress, phone, email) VALUES('" . $_POST['clubName'] . "," . $_POST['clubDescription'] . "," . $mediaID . "," . $_POST['pname'] . "," .  $_POST['adress'] . "," .  $_POST['phone'] . "," .  $_POST['email'] . "'')";
 			if ($db->query($sql_query) === TRUE) {
 			    echo "New record created successfully";
 			} else {

da37feb35e63f560b49f1aaabbc85ed221d99090
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-08 00:52:43 +0000

    Create test.php

---
+++ app/test.php
@@ -0,0 +1,23 @@
+<?php
+    // imports the header/navigation
+    include("inc/header.inc");
+    include("./dbconnect.php");
+?>
+  <section>
+		<div id="BodyText">
+    			<?php
+        			echo "Welcome to GoPortlethean. We are an collection of local and progressive sports clubs who are working together to develop safe and fun sport & fitness activities in the Portlethen area. We work with sportscotland and other organisations to develop our clubs. Our website is a single access point to find out more about the fantastic sporting opportunities in our area.";
+     			?>
+			<br>
+			<img src="images/IndexImage1.png" alt="Indeximage1.png" height="150" width="150">
+			<img src="images/IndexImage2.png" alt="Indeximage2.png" height="150" width="150">
+			<img src="images/IndexImage3.png" alt="Indeximage3.png" height="150" width="150">
+
+
+		</div>
+</section>
+
+<?php
+    // imports the footer
+    include("inc/footer.inc");
+?>

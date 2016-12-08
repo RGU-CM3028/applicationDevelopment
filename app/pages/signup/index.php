a8eaf5222cb786d552badd5996d4e907874ad7cd
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-08 02:38:11 +0000

    Update index.php

--- app/pages/signup/index.php
+++ app/pages/signup/index.php
@@ -1,20 +1,27 @@
- <?
-     // imports the header/navigation
-    include("../../inc/header2.inc");
-    include('../../dbconnect.php');
+<?
     //This connects the database and continues the session onto this page.
-    session_start();	
+    session_start();
+    include("../dbconnect.php");
+	
     //This prepares the accesslevel.
-    $accesslevel = $_SESSION['userType'];	
+    $accesslevel = $_SESSION['userType'];
+	
     //This displays the function contents	
-	    if ($accesslevel == "admin"  || $accesslevel == "reader" || $accesslevel == "NKPAG") {		    
+    displayAccessLevelInformation($accesslevel);
+	
+    //This is the function doing its magic on a set piece of code.	
+    function displayAccessLevelInformation($accesslevel){
+	    if ($accesslevel == "admin"  || $accesslevel == "reader" || $accesslevel == "NKPAG") {
+		    
 		    //This sends already signed in users back to the index page
 		    header("location:../index.php");
 		    die();
 	    } else {
-            ?>	    
+            ?>
+	    
 	    <!--This is the header for the form-->
-            <h1>Signup Form</h1>	
+            <h1>Signup Form</h1>
+	
             <?
             //This is used to get the error message associated to the error code.
             if (isset($_GET['space'])){
@@ -30,6 +37,7 @@
                 echo "<p><font color='red'>Username already taken.</font></p>";
             }
             ?>
+    
             <!-- This is the form used for users to sign up -->
             <form method="post" action="signup.php">
                 <p><input type="text" name="username" value="" placeholder="Username please"></p>
@@ -37,9 +45,10 @@
                 <p><input type="password" name="passwordcheck" value="" placeholder="Confirm Password please"></p>
                 <p class="submit"><input type="submit" name="commit" value="Login"></p>
             </form>
+    
+            <!-- This is used to return to the login screen-->
+            <a href="index.php">Return to login screen</a>
             <?
         }
     }
-    // imports the footer
-    include("../../inc/footer.inc");
-?>
+    ?> 

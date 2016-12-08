f9f396aef97534e6edfb613e23380051a56ce39d
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-08 12:36:08 +0000

    Update checklogin.php

--- app/login/checklogin.php
+++ app/login/checklogin.php
@@ -43,6 +43,10 @@ $result = $db->query($boom);
 while($row = $result->fetch_array()){
 $userType = $row['userType'];
 }   
+echo $mypassword;
+echo $myusername;
+echo $checker;
+die();
 //This checks if any pairs matched or not. And send the user back to the index page. 
 //If the user managed to log in the username and usertype is saved as a session.
 if($checker==1){

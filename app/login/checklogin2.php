90053379ac4e7e7d79c13a5d539eeded66a52318
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-08 13:22:07 +0000

    Update checklogin2.php

--- app/login/checklogin2.php
+++ app/login/checklogin2.php
@@ -26,9 +26,9 @@ echo $_POST["password"]."<br>";
 
 //Security checks Version1
 $myusername = stripslashes($myusername);
-$myusername = filter_var($myusername, FILTER_SANITIZE_STRING);
-$myusername = htmlspecialchars($myusername, ENT_QUOTES, "ISO-8859-1");
-//$myusername = mysqli_real_escape_string($db,$myusername);
+//$myusername = filter_var($myusername, FILTER_SANITIZE_STRING);
+//$myusername = htmlspecialchars($myusername, ENT_QUOTES, "ISO-8859-1");
+$myusername = mysqli_real_escape_string($db,$myusername);
 $mypassword = stripslashes($mypassword);
 $mypassword = mysqli_real_escape_string($db,$mypassword);
 $salt = "qwertgfdert45t456545655";

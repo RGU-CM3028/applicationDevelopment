6a0bdc3405d768c63c30272a4d0739de2045aa67
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-08 17:34:10 +0000

    Update upload.php

--- app/fileupload/upload.php
+++ app/fileupload/upload.php
@@ -4,20 +4,13 @@
       //move_uploaded_file($_FILES['filetoupload']['tmp_name'], $to);
       //echo "Uploaded";
 //}
-      
-$file_result = "";
-if ($_FILES["fileToUpload"]["error"] > 0) {
-      $file_result.= "No file uploaded. so on";
-      $file_result.= "Error code:" . $_FILES["fileToUpload"]["error"]."<br>";
-}else{
-      $file_result .=
-            "Upload: ". $_FILES["fileToUpload"]["name"]."<br>".
-            "Type: ". $_FILES["fileToUpload"]["type"]."<br>".
-            "Size: ". $_FILES["fileToUpload"]["size"]/1024." Kb<br>".
-            "Temp file: ". $_FILES["fileToUpload"]["tmp_name"]."<br>";
-      move_uploaded_file($_FILES['filetoupload']['tmp_name'],
-                         "uploads/".$_FILES['filetoupload']['name']);
-      $file_result .= "File Upload Successful!"
+if(isset($_POST['upload'])){      
+      echo $image_name = $_FILES["file"]["name"];
+      echo $image_type = $_FILES["file"]["type"];
+      echo $image_size = $_FILES["file"]["size"];
+      echo $image_tmp_name = $_FILES["file"]["tmp_name"];
 }
+
+
                          
 ?>

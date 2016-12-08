fa246fbf63914b98beb8571a2dc0f1b7412e0aa6
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-08 17:42:35 +0000

    Update index.php

--- app/fileupload/index.php
+++ app/fileupload/index.php
@@ -2,5 +2,5 @@
 <form action="upload.php" method="post" enctype="multipart/form-data">
     Select image to upload:
     <input type="file" name="file" id="file">
-    <input type="upload" value="upload" name="upload">
+    <input type="submit" value="upload" name="upload">
 </form>

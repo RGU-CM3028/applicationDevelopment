b24a27f2c410e086af99c68035587419fe0b349d
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-08 02:35:54 +0000

    Update index.php

--- app/pages/signup/index.php
+++ app/pages/signup/index.php
@@ -1,7 +1,4 @@
  <?
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
      // imports the header/navigation
     include("../../inc/header2.inc");
     include('../../dbconnect.php');

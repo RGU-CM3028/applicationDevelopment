57f9cb9758abdf2a0b087fa5974009f064c093c0
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-08 03:59:28 +0000

    Update signup.php

--- app/pages/signup/signup.php
+++ app/pages/signup/signup.php
@@ -1,6 +1,8 @@
 <?php
+
 //This connects the database here.
 include("../dbconnect.php");
+
 //html change safety check. This is to catch out any attempt to change variables and so on in the html.
 $myusername = "";
 $mypassword = "";
@@ -23,10 +25,16 @@ if(isset($_POST['passwordcheck'])) {
     header("location:index.php");
     die();
 }
+
 //This is the fields from the signup form.
 $myusername = $_POST["username"];
 $mypassword = $_POST["password"];
 $passwordcheck = $_POST["passwordcheck"];
+
+echo $myusername;
+echo $mypassword;
+echo $passcheck;
+die();
 //Security checking V1.
 $myusername = stripslashes($myusername);
 $myusername = mysqli_real_escape_string($db, $myusername);
@@ -41,13 +49,15 @@ $passwordcheck = $passwordcheck.$salt;
 $passwordcheck = hash('sha256', $passwordcheck);
 
 echo $myusername;
-    echo $mypassword;
-    echo $passcheck;
+echo $mypassword;
+echo $passcheck;
 die();
+
 //This declairs the boolians so they dont cause an error
 $userspace = 'false';
 $passspace = 'false';
 $pass2space = 'false';
+
 //This checks to see if their is any spaces in the variables
 if (strpos($myusername, ' ') !== false) {
     $userspace = 'true';
@@ -58,17 +68,20 @@ if (strpos($mypassword, ' ') !== false) {
 if (strpos($passwordcheck, ' ') !== false) {
     $pass2space ='true';
 }
+
 //This tests to see if there is any spaces in the text
 if($userspace=='true' || $passspace=='true' || $pass2space=='true') {
     header("location:index.php?space=1");
     die();
 }
+
 //This checks to see if the fields are empty or not.
 if(empty($myusername) || empty($mypassword) || empty($passwordcheck))
     {
     header("location:index.php?empty=1");
     die();
 }
+
 //This checks to see if the username is taken or not.
 $dup = mysqli_query($db, "SELECT username FROM users WHERE username='$myusername'");
 $userchecker = mysqli_fetch_assoc($dup);
@@ -76,6 +89,7 @@ if(mysqli_num_rows($dup) >0){
     header("location:index.php?dup=1");
     die();
 } 
+
 //This compares the passwords. If the match then the user is created. If not then the user is told to check again.
 if($mypassword==$passwordcheck) {
     $sql = "INSERT INTO users (username, password, userType) VALUES ('". $myusername ."', '" .$mypassword."', 'reader')";

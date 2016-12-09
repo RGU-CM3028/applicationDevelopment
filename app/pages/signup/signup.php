83a06ebaf3ffb222a426060c1f827ab720d1b67c
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-09 14:26:05 +0000

    Update signup.php

--- app/pages/signup/signup.php
+++ app/pages/signup/signup.php
@@ -28,23 +28,39 @@ if(isset($_POST['passwordcheck'])) {
 $myusername = $_POST["username"];
 $mypassword = $_POST["password"];
 $passwordcheck = $_POST["passwordcheck"];
+
+ echo $myusername;
+echo $mypassword;
+echo $passwordcheck;
+
 //Security checking V1.
 $myusername = stripslashes($myusername);
 $myusername = filter_var($myusername, FILTER_SANITIZE_STRING);
 $myusername = htmlspecialchars($myusername, ENT_QUOTES, "ISO-8859-1");
 $mypassword = stripslashes($mypassword);
-$mypassword = mysqli_real_escape_string($db, $mypassword);
+$mypassword = filter_var($mypassword, FILTER_SANITIZE_STRING);
+$mypassword = htmlspecialchars($mypassword, ENT_QUOTES, "ISO-8859-1");
 $passwordcheck = stripslashes($passwordcheck);
-$passwordcheck = mysqli_real_escape_string($db, $passwordcheck);
+$passwordcheck = filter_var($passwordcheck, FILTER_SANITIZE_STRING);
+$passwordcheck = htmlspecialchars($passwordcheck, ENT_QUOTES, "ISO-8859-1")
+    
+ echo $myusername;
+echo $mypassword;
+echo $passwordcheck;
+die();
+    
+//encrypt 
 $salt = "qwertgfdert45t456545655";
 $mypassword = $mypassword.$salt;
 $mypassword = hash('sha256', $mypassword);
 $passwordcheck = $passwordcheck.$salt;
 $passwordcheck = hash('sha256', $passwordcheck);
+
 //This declairs the boolians so they dont cause an error
 $userspace = 'false';
 $passspace = 'false';
 $pass2space = 'false';
+
 //This checks to see if their is any spaces in the variables
 if (strpos($myusername, ' ') !== false) {
     $userspace = 'true';
@@ -55,17 +71,20 @@ if (strpos($mypassword, ' ') !== false) {
 if (strpos($passwordcheck, ' ') !== false) {
     $pass2space ='true';
 }
+
 //This tests to see if there is any spaces in the text
 if($userspace=='true' || $passspace=='true' || $pass2space=='true') {
     header("location:index.php?space=1");
     die();
 }
+
 //This checks to see if the fields are empty or not.
 if(empty($myusername) || empty($mypassword) || empty($passwordcheck))
     {
     header("location:index.php?empty=1");
     die();
 }
+
 //This checks to see if the username is taken or not.
 $dup = mysqli_query($db, "SELECT username FROM users WHERE username='$myusername'");
 $userchecker = mysqli_fetch_assoc($dup);

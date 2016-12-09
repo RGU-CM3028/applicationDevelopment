4d1c7f41a16a46cd1094c5f9adfd25938d86317b
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-09 14:32:27 +0000

    Update signup.php

--- app/pages/signup/signup.php
+++ app/pages/signup/signup.php
@@ -28,11 +28,9 @@ if(isset($_POST['passwordcheck'])) {
 $myusername = $_POST["username"];
 $mypassword = $_POST["password"];
 $passwordcheck = $_POST["passwordcheck"];
-
- echo $myusername;
+echo $myusername;
 echo $mypassword;
-echo $passwordcheck;
-
+echo $passwordcheck. "<br>";
 //Security checking V1.
 $myusername = stripslashes($myusername);
 $myusername = filter_var($myusername, FILTER_SANITIZE_STRING);
@@ -41,25 +39,26 @@ $mypassword = stripslashes($mypassword);
 $mypassword = filter_var($mypassword, FILTER_SANITIZE_STRING);
 $mypassword = htmlspecialchars($mypassword, ENT_QUOTES, "ISO-8859-1");
 $passwordcheck = stripslashes($passwordcheck);
-$passwordcheck = mysqli_real_escape_string($db, $passwordcheck);
-    
- echo $myusername;
-echo $mypassword;
-echo $passwordcheck;
-die();
-    
-//encrypt 
-$salt = "qwertgfdert45t456545655";
-$mypassword = $mypassword.$salt;
-$mypassword = hash('sha256', $mypassword);
-$passwordcheck = $passwordcheck.$salt;
-$passwordcheck = hash('sha256', $passwordcheck);
-
+$passwordcheck = filter_var($passwordcheck, FILTER_SANITIZE_STRING);
+$passwordcheck = htmlspecialchars($passwordcheck, ENT_QUOTES, "ISO-8859-1");
+//$passwordcheck = mysqli_real_escape_string($db, $passwordcheck);
 //This declairs the boolians so they dont cause an error
 $userspace = 'false';
 $passspace = 'false';
 $pass2space = 'false';
-
+//This checks if the user tied to php inject
+if (strpos($myusername, '&#') !== false) {
+    $myusername = ' ';
+    $userspace = 'true';
+}
+if (strpos($mypassword, '&#') !== false) {
+    $mypassword = ' ';
+    $passspace = 'true';
+}
+if (strpos($passwordcheck, '&#') !== false) {
+    $passwordcheck = ' ';
+    $pass2space ='true';
+}
 //This checks to see if their is any spaces in the variables
 if (strpos($myusername, ' ') !== false) {
     $userspace = 'true';
@@ -70,20 +69,27 @@ if (strpos($mypassword, ' ') !== false) {
 if (strpos($passwordcheck, ' ') !== false) {
     $pass2space ='true';
 }
-
+echo $myusername;
+echo $mypassword;
+echo $passwordcheck. "<br>";
+die();
 //This tests to see if there is any spaces in the text
 if($userspace=='true' || $passspace=='true' || $pass2space=='true') {
     header("location:index.php?space=1");
     die();
 }
-
+//This encrypts the password
+$salt = "qwertgfdert45t456545655";
+$mypassword = $mypassword.$salt;
+$mypassword = hash('sha256', $mypassword);
+$passwordcheck = $passwordcheck.$salt;
+$passwordcheck = hash('sha256', $passwordcheck);
 //This checks to see if the fields are empty or not.
 if(empty($myusername) || empty($mypassword) || empty($passwordcheck))
     {
     header("location:index.php?empty=1");
     die();
 }
-
 //This checks to see if the username is taken or not.
 $dup = mysqli_query($db, "SELECT username FROM users WHERE username='$myusername'");
 $userchecker = mysqli_fetch_assoc($dup);
@@ -109,3 +115,4 @@ if($mypassword==$passwordcheck) {
     die();
 }
 ?>
+

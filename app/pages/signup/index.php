0193f5725a01d49693d31677c3d0d404561882cd
Roulette Rogue <1603964@rgu.ac.uk>
2016-12-09 12:12:58 +0000

    design changes

--- app/pages/signup/index.php
+++ app/pages/signup/index.php
@@ -4,44 +4,50 @@
     session_start();
     include("../dbconnect.php");
 	
-$usertypeholder1 = 'admin';
-$usertypeholder2 = 'reader';
-$usertypeholder3 = 'NKPAG';
-   if ($usertypeholde1 = $_SESSION['userType'] || $usertypeholder2 = $_SESSION['userType'] || $usertypeholder3 = $_SESSION['userType'] ){
-	header("location:../../index.php");
-	die();
-   } else {
-	   //safe
-   }
+    $usertypeholder1 = 'admin';
+    $usertypeholder2 = 'reader';
+    $usertypeholder3 = 'NKPAG';
+   
+    if ($usertypeholde1 = $_SESSION['userType'] || $usertypeholder2 = $_SESSION['userType'] || $usertypeholder3 = $_SESSION['userType'] )
+    {
+	   header("location:../../index.php");
+	   die();
+    } else 
+    {
+        //safe
+    }
 ?>
-	    
-	    <!--This is the header for the form-->
-            <h1>Signup Form</h1>
+    <section>
+	    <div class="register-box">
+            <!--This is the header for the form-->
+                <h1>Signup Form</h1>
 	
             <?
-            //This is used to get the error message associated to the error code.
-            if (isset($_GET['space'])){
-                echo "<p><font color='red'>Please ensure you dont use spaces in your username or password.</font></p>";
-            }
-            if (isset($_GET['empty'])){
-                echo "<p><font color='red'>Please ensure you fill all the fields.</font></p>";
-            }
-            if (isset($_GET['same'])){
-                echo "<p><font color='red'>Please ensure the password fields match.</font></p>";
-            }
-            if (isset($_GET['dup'])){
-                echo "<p><font color='red'>Username already taken.</font></p>";
-            }
+                //This is used to get the error message associated to the error code.
+                if (isset($_GET['space'])){
+                    echo "<p><font color='red'>Please ensure you dont use spaces in your username or password.</font></p>";
+                }
+                if (isset($_GET['empty'])){
+                    echo "<p><font color='red'>Please ensure you fill all the fields.</font></p>";
+                }
+                if (isset($_GET['same'])){
+                    echo "<p><font color='red'>Please ensure the password fields match.</font></p>";
+                }
+                if (isset($_GET['dup'])){
+                    echo "<p><font color='red'>Username already taken.</font></p>";
+                }
             ?>
     
             <!-- This is the form used for users to sign up -->
-	<form method="post" action="signup.php">				
-		<p><label>Username</label><input type="text" name="username" maxlength="30"></p>
-		<p></p>
-		<p><label>Password</label><input type="password" name="password" maxlength="25"></p>
-		<p></p>
-		<p><label>Password Check</label><input type="password" name="passwordcheck" maxlength="25"></p>
-		 <p class="submit"><input type="submit" name="commit" value="Login"></p>
-	<?
-  include("../../inc/footer.inc");
-    ?> 
+            <form name="register" method="post" action="signup.php">				
+                <label>Username</label><input type="text" name="username" maxlength="30">
+                <label>Password</label><input type="password" name="password" maxlength="25">
+                <label>Password Check</label><input type="password" name="passwordcheck" maxlength="25">
+                <input type="submit" name="commit" value="Login">
+            </form>
+        </div>
+    </section>
+<?
+    include("../../inc/footer.inc");
+?> 
+

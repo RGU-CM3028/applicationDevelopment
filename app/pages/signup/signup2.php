01ac4a145e8ba24e43698143ca7b7791260db0b1
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-10 17:32:37 +0000

    Update signup2.php

--- app/pages/signup/signup2.php
+++ app/pages/signup/signup2.php
@@ -69,6 +69,7 @@ if(empty($myusername) || empty($mypassword) || empty($passwordcheck))
 $dup = $db->prepare("SELECT username FROM users WHERE username=?");
 $dup->bind_param("s", $myusername);
 $dup->execute(); 
+$dup->fetch();
 //$dup = mysqli_query($db, "SELECT username FROM users WHERE username='$myusername'");
 $userchecker = mysqli_fetch_assoc($dup);
 if(mysqli_num_rows($dup) >0){

0995d48ab61bad42c1544dbf1bde7d7da7fdbec6
florian peyron <flotos@hotmail.fr>
2016-12-10 00:12:39 +0000

    POST

--- app/pages/health/edition.php
+++ app/pages/health/edition.php
@@ -4,7 +4,7 @@ File handling the creation and edition of news
 
 	<?php
 
-    var_dump($_POST)
+    var_dump($_POST);
 		include("../../dbconnect.php");
 
 		//If we are editing instead of creating a new

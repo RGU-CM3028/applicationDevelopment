8bbd7152ee016bfa4f8260ba5a4bb6a49be60bd0
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-10 13:49:20 +0000

    Update signup2.php

--- app/pages/signup/signup2.php
+++ app/pages/signup/signup2.php
@@ -2,23 +2,6 @@
 //This connects the database here.
 include("../../dbconnect.php");
 
-//Good for login and so on. Not for here		
- function sanitize($data)		
- {		
-     include("../../dbconnect.php");		
-     // apply stripslashes if magic_quotes_gpc is enabled		
-     if(get_magic_quotes_gpc())		
-     {		
-         $data = stripslashes($data);		
-     }		
-     // a mySQL connection is required before using this function		
-     $data = filter_var($data, FILTER_SANITIZE_STRING);		
-     $data = preg_replace('/[^a-z0-9\s]/i', '', $data);		
-     //$data = mysqli_real_escape_string($db, $data);		
-     return $data;		
- }		
- 
-
 //html change safety check. This is to catch out any attempt to change variables and so on in the html.
 $myusername = "";
 $mypassword = "";
@@ -47,7 +30,7 @@ $mypassword = $_POST["password"];
 $passwordcheck = $_POST["passwordcheck"];
 $myusertype = 'reader';
 
-echo $myysername;
+echo $myusername;
 die();
 
 //Security checking V1.

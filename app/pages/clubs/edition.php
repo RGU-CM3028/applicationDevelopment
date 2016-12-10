aae2246964a47fc92cc42b861ff4ddc8948e4bbc
florian peyron <flotos@hotmail.fr>
2016-12-10 15:31:35 +0000

    LiveDebugging - edition.php

--- app/pages/clubs/edition.php
+++ app/pages/clubs/edition.php
@@ -103,10 +103,4 @@ File handling the creation and edition of clubs
     }
     ?>
 	</form>
-
-    <form action="upload.php" method="post" enctype="multipart/form-data">
-      Select image to upload:
-      <input type="file" name="fileToUpload" id="fileToUpload">
-      <input type="submit" value="Upload Image" name="submit">
-  </form>
 </body>

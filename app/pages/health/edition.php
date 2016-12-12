77c7633a55058cfb0e9bb8c1681ab0cb7e779b29
florian peyron <flotos@hotmail.fr>
2016-12-12 18:55:23 +0000

    Map and health edition style

--- app/pages/health/edition.php
+++ app/pages/health/edition.php
@@ -2,79 +2,79 @@
 File handling the creation and edition of news
 -->
 
-	<?php
+<?php
 
-		include("../../dbconnect.php");
-		include("../../inc/header.inc");
-    echo "<section>
-    <div class='edition'>";
-    if(!(isset($_SESSION['userType']))) {
+	include("../../dbconnect.php");
+	include("../../inc/header.inc");
+  echo "<section>
+  <div class='edition'>";
+  if(!(isset($_SESSION['userType']))) {
+    header('location:./');
+  } else if($_SESSION['userType'] !== 'admin') {
       header('location:./');
-    } else if($_SESSION['userType'] !== 'admin') {
-        header('location:./');
+  }
+	//If we are editing instead of creating a new
+  // if HWNewsID setted, we update
+	if(isset($_GET['HWNewsID'])){
+    echo "<h1 class='editTitle'> Update your news </h1> <br>";
+		$sql_query = "SELECT HWNewsName, HWNewsText FROM hwnews WHERE HWNewsID = " . $_GET['HWNewsID'];
+		// Most insecure line ever, will patch if we have additionnal time after site finished. Paradise for sql injection.
+		$queryResult = $db->query($sql_query);
+    while ($row = $queryResult->fetch_array()) {
+      $title = $row['HWNewsName'];
+			$description = $row['HWNewsText'];
     }
-		//If we are editing instead of creating a new
-    // if HWNewsID setted, we update
-		if(isset($_GET['HWNewsID'])){
-      echo "<h1 class='editTitle'> Update your news </h1> <br>";
-			$sql_query = "SELECT HWNewsName, HWNewsText FROM hwnews WHERE HWNewsID = " . $_GET['HWNewsID'];
-			// Most insecure line ever, will patch if we have additionnal time after site finished. Paradise for sql injection.
-			$queryResult = $db->query($sql_query);
-      while ($row = $queryResult->fetch_array()) {
-        $title = $row['HWNewsName'];
-				$description = $row['HWNewsText'];
-      }
-    } else {
-      echo "<h1 class='editTitle'> Create a news </h1> <br>";
-      //Initialise the fields
-      $title = "";
-      $description = "";
+  } else {
+    echo "<h1 class='editTitle'> Create a news </h1> <br> ";
+    //Initialise the fields
+    $title = "";
+    $description = "";
+  }
+
+  if(isset($_POST['submitAdd'])) {
+    $sql_query = 'INSERT INTO HWNews (HWNewsDate, HWNewsName, HWNewsText)
+			 VALUES (
+				 CURDATE(), "'
+    . mysqli_real_escape_string($db, $_POST['HWNewsName']) . '","'
+    . mysqli_real_escape_string($db, $_POST['HWNewsText']) . '");';
+
+    if ($db->query($sql_query) === TRUE) {
+    	    header("location:index.php");
+    	} else {
+    	    echo "Error: " . $sql_query . "<br>" . $db->error;
+    	}
+  }
+  if(isset($_POST['submitUpdate'])) {
+      $sql_query = 'UPDATE HWNews
+      SET HWNewsName="'.mysqli_real_escape_string($db, $_POST['HWNewsName']).'",
+      HWNewsText="'.mysqli_real_escape_string($db, $_POST['HWNewsText']).'"
+			WHERE HWNewsID='.$_GET['HWNewsID'];
+
+      if ($db->query($sql_query) == TRUE) {
+            header("location:index.php");
+        } else {
+            echo "Error: " . $sql_query . "<br>" . $db->error;
+        }
     }
+?>
+  	<form action="" method="POST">
+      <div class='editContent'>
+    		Title : <br>
+        <input type="text" name="HWNewsName" value=<?php echo "\"" . $title . "\"";?>><br>
+    		Description : <br>
+        <textarea name="HWNewsText" rows="5" cols="40"><?php echo $description;?></textarea><br>
 
-    if(isset($_POST['submitAdd'])) {
-      $sql_query = 'INSERT INTO HWNews (HWNewsDate, HWNewsName, HWNewsText)
-				 VALUES (
-					 CURDATE(), "'
-      . mysqli_real_escape_string($db, $_POST['HWNewsName']) . '","'
-      . mysqli_real_escape_string($db, $_POST['HWNewsText']) . '");';
-
-      if ($db->query($sql_query) === TRUE) {
-      	    header("location:index.php");
-      	} else {
-      	    echo "Error: " . $sql_query . "<br>" . $db->error;
-      	}
-    }
-    if(isset($_POST['submitUpdate'])) {
-        $sql_query = 'UPDATE HWNews
-        SET HWNewsName="'.mysqli_real_escape_string($db, $_POST['HWNewsName']).'",
-        HWNewsText="'.mysqli_real_escape_string($db, $_POST['HWNewsText']).'"
-				WHERE HWNewsID='.$_GET['HWNewsID'];
-
-        if ($db->query($sql_query) == TRUE) {
-              header("location:index.php");
-          } else {
-              echo "Error: " . $sql_query . "<br>" . $db->error;
-          }
-      }
-	?>
-    	<form action="" method="POST">
-        <div class='editContent'>
-      		Title : <br>
-          <input type="text" name="HWNewsName" value=<?php echo "\"" . $title . "\"";?>><br>
-      		Description : <br>
-          <textarea name="HWNewsText" rows="5" cols="40"><?php echo $description;?></textarea><br>
-
-          <? if(isset($_GET['HWNewsID'])) {
-            echo "<input class='backButton'  type='submit' name='submitUpdate' value='Update news'>";
-          } else {
-            echo "<input class='backButton'  type='submit' name='submitAdd' value='Add news'>";
-          }
-          ?>
-      		<a id='backEdition' class='backButton' href='./'>Back to Health and Wellbeing</a>
-        </div>
-    	</form>
-    </div>
-  </section>
+        <? if(isset($_GET['HWNewsID'])) {
+          echo "<input class='backButton'  type='submit' name='submitUpdate' value='Update news'>";
+        } else {
+          echo "<input class='backButton'  type='submit' name='submitAdd' value='Add news'>";
+        }
+        ?>
+    		<a class='backButton' href='./'>Back to Health and Wellbeing</a>
+      </div>
+  	</form>
+  </div>
+</section>
 
 <?
 include('../../inc/footer.inc');
--- app/pages/map/edition.php
+++ app/pages/map/edition.php
@@ -2,181 +2,184 @@
 File handling the creation and edition of points, area and path
 -->
 
-	<?php
-		include("../../dbconnect.php");
-    include("../../inc/header.inc");
+<?php
+	include("../../dbconnect.php");
+  include("../../inc/header.inc");
+
+  echo "<section>
+  <div class='edition'>";
 
 
-
-    if(!(isset($_SESSION['userType']))) {
+  if(!(isset($_SESSION['userType']))) {
+    header('location:./');
+  } else if($_SESSION['userType'] !== 'NKAPG' &&
+  $_SESSION['userType'] !== 'admin') {
       header('location:./');
-    } else if($_SESSION['userType'] !== 'NKAPG' &&
-    $_SESSION['userType'] !== 'admin') {
-        header('location:./');
-    }
+  }
 
-      
+    
 
-		//If we are editing/Deleting instead of creating a new
-    // if id is set, we are updating
-		if(isset($_GET['id'])){
-      //Updating
-      //Fill as the form values with the actual ones
-      echo "<h1> Update the element </h1>";
+	//If we are editing/Deleting instead of creating a new
+  // if id is set, we are updating
+	if(isset($_GET['id'])){
+    //Updating
+    //Fill as the form values with the actual ones
+    echo "<h1> Update the element </h1> <br>";
 
-      if(isset($_GET['point'])){
-        if(isset($_GET['delete'])){
-          $sql_query="delete from GeoPoint
-          where pointID=" . $_GET['id'];
-          if($db->query($sql_query) === TRUE) {
-            header('location:./index.php');
-          } else {
-              echo "Error: " . $sql_query . "<br>" . $db->error;
-          }
+    if(isset($_GET['point'])){
+      if(isset($_GET['delete'])){
+        $sql_query="delete from GeoPoint
+        where pointID=" . $_GET['id'];
+        if($db->query($sql_query) === TRUE) {
+          header('location:./index.php');
         } else {
-    			$sql = "SELECT pointID, pointType, pointDescription, coordinateX, coordinateY FROM GeoPoint WHERE pointID = " . $_GET['id'];
-          $points = $db->query($sql);
-
-          while ($row = $points->fetch_array()) {
-            $id = $_GET['id'];
-    				$type = $row['pointType'];
-    				$description = $row['pointDescription'];
-    				$coordinateX = $row['coordinateX'];
-    				$coordinateY = $row['coordinateY'];
-          }
+            echo "Error: " . $sql_query . "<br>" . $db->error;
         }
-      } elseif (isset($_GET['path'])) {
-        if(isset($_GET['delete'])){
-          $sql_query="delete from GeoPath
-          where pathID=" . $_GET['id'];
-          if($db->query($sql_query) === TRUE) {
-            header('location:./index.php');
-          } else {
-              echo "Error: " . $sql_query . "<br>" . $db->error;
-          }
-        } else {
-          $sql = "SELECT pathID, pathType, pathDescription, vectors FROM Geopath WHERE pathID = " . $_GET['id'];
-          $paths = $db->query($sql);
+      } else {
+  			$sql = "SELECT pointID, pointType, pointDescription, coordinateX, coordinateY FROM GeoPoint WHERE pointID = " . $_GET['id'];
+        $points = $db->query($sql);
 
-          while ($row = $paths->fetch_array()) {
-            $id = $_GET['id'];
-            $type = $row['pathType'];
-            $description = $row['pathDescription'];
-            $vectors = $row['vectors'];
-          }
-        }
-      } elseif (isset($_GET['area'])) {
-        if(isset($_GET['delete'])){
-          $sql_query="delete from GeoArea
-          where areaID=" . $_GET['id'];
-          if($db->query($sql_query) === TRUE) {
-            header('location:./index.php');
-          } else {
-              echo "Error: " . $sql_query . "<br>" . $db->error;
-          }
-        } else {
-          $sql = "SELECT areaID, areaType, areaDescription, vectors FROM Geoarea WHERE areaID = " . $_GET['id'];
-          $areas = $db->query($sql);
-
-          while ($row = $areas->fetch_array()) {
-            $id = $_GET['id'];
-            $type = $row['areaType'];
-            $description = $row['areaDescription'];
-            $vectors = $row['vectors'];
-          }
+        while ($row = $points->fetch_array()) {
+          $id = $_GET['id'];
+  				$type = $row['pointType'];
+  				$description = $row['pointDescription'];
+  				$coordinateX = $row['coordinateX'];
+  				$coordinateY = $row['coordinateY'];
         }
       }
-    } else {
-      //New element
-      //Initialise the fields
-      echo "<h1> Create a new element </h1>";
-      $id = "";
-      $type = "";
-      $description = "";
-      $coordinateX = "";
-      $coordinateY = "";
-    }
-
-    if(isset($_POST['submitAdd'])) {
-      if(isset($_POST['point'])){
-        $sql_query = "INSERT INTO GeoPoint (pointType, pointDescription, coordinateX, coordinateY ) VALUES ('"
-        . $_POST['type'] . "',' "
-        . $_POST['description'] . "',' "
-        . $_POST['coordinateX'] . "',' "
-        . $_POST['coordinateY'] . "');";
-
-        if ($db->query($sql_query) === TRUE) {
-           header("location:index.php");
-      	} else {
-      	    echo "Error: " . $sql_query . "<br>" . $db->error;
-      	}
-      } elseif(isset($_POST['path'])){
-        $sql_query = "INSERT INTO Geopath (pathType, pathDescription, vectors ) VALUES ('"
-        . $_POST['type'] . "',' "
-        . $_POST['description'] . "',' "
-        . $_POST['vectors'] . "');";
-
-        if ($db->query($sql_query) === TRUE) {
-           header("location:index.php");
+    } elseif (isset($_GET['path'])) {
+      if(isset($_GET['delete'])){
+        $sql_query="delete from GeoPath
+        where pathID=" . $_GET['id'];
+        if($db->query($sql_query) === TRUE) {
+          header('location:./index.php');
         } else {
             echo "Error: " . $sql_query . "<br>" . $db->error;
         }
-      } elseif(isset($_POST['area'])){
-        $sql_query = "INSERT INTO Geoarea (areaType, areaDescription, vectors ) VALUES ('"
-        . $_POST['type'] . "',' "
-        . $_POST['description'] . "',' "
-        . $_POST['vectors'] . "');";
+      } else {
+        $sql = "SELECT pathID, pathType, pathDescription, vectors FROM Geopath WHERE pathID = " . $_GET['id'];
+        $paths = $db->query($sql);
 
-        if ($db->query($sql_query) === TRUE) {
-           header("location:index.php");
+        while ($row = $paths->fetch_array()) {
+          $id = $_GET['id'];
+          $type = $row['pathType'];
+          $description = $row['pathDescription'];
+          $vectors = $row['vectors'];
+        }
+      }
+    } elseif (isset($_GET['area'])) {
+      if(isset($_GET['delete'])){
+        $sql_query="delete from GeoArea
+        where areaID=" . $_GET['id'];
+        if($db->query($sql_query) === TRUE) {
+          header('location:./index.php');
         } else {
             echo "Error: " . $sql_query . "<br>" . $db->error;
+        }
+      } else {
+        $sql = "SELECT areaID, areaType, areaDescription, vectors FROM Geoarea WHERE areaID = " . $_GET['id'];
+        $areas = $db->query($sql);
+
+        while ($row = $areas->fetch_array()) {
+          $id = $_GET['id'];
+          $type = $row['areaType'];
+          $description = $row['areaDescription'];
+          $vectors = $row['vectors'];
         }
       }
     }
-    if(isset($_POST['submitUpdate'])) {
-      if(isset($_POST['point'])){
-        $sql_query = "UPDATE GeoPoint SET
-        pointType='".$_POST['type']."',
-        pointDescription='".$_POST['description']."',
-        coordinateX=".$_POST['coordinateX'].",
-        coordinateY=".$_POST['coordinateY']."
-         WHERE pointID=".$_GET['id'];
+  } else {
+    //New element
+    //Initialise the fields
+    echo "<h1> Create a new element </h1> <br>";
+    $id = "";
+    $type = "";
+    $description = "";
+    $coordinateX = "";
+    $coordinateY = "";
+  }
 
-        if($db->query($sql_query) === TRUE) {
-          header("location:index.php");
-        } else {
-            echo "Error: " . $sql_query . "<br>" . $db->error;
-        }
-      } elseif(isset($_POST['path'])){
-        $sql_query = "UPDATE Geopath SET
-        pathType='".$_POST['type']."',
-        pathDescription='".$_POST['description']."',
-        vectors='".$_POST['vectors']."'
-         WHERE pathID=".$_GET['id'];
+  if(isset($_POST['submitAdd'])) {
+    if(isset($_POST['point'])){
+      $sql_query = "INSERT INTO GeoPoint (pointType, pointDescription, coordinateX, coordinateY ) VALUES ('"
+      . $_POST['type'] . "',' "
+      . $_POST['description'] . "',' "
+      . $_POST['coordinateX'] . "',' "
+      . $_POST['coordinateY'] . "');";
 
-        if($db->query($sql_query) === TRUE) {
-          header("location:index.php");
-        } else {
-            echo "Error: " . $sql_query . "<br>" . $db->error;
-        }
-      }elseif(isset($_POST['area'])){
-        $sql_query = "UPDATE Geoarea SET
-        areaType='".$_POST['type']."',
-        areaDescription='".$_POST['description']."',
-        vectors='".$_POST['vectors']."'
-         WHERE areaID=".$_GET['id'];
+      if ($db->query($sql_query) === TRUE) {
+         header("location:index.php");
+    	} else {
+    	    echo "Error: " . $sql_query . "<br>" . $db->error;
+    	}
+    } elseif(isset($_POST['path'])){
+      $sql_query = "INSERT INTO Geopath (pathType, pathDescription, vectors ) VALUES ('"
+      . $_POST['type'] . "',' "
+      . $_POST['description'] . "',' "
+      . $_POST['vectors'] . "');";
 
-        if($db->query($sql_query) === TRUE) {
-          header("location:index.php");
-        } else {
-            echo "Error: " . $sql_query . "<br>" . $db->error;
-        }
+      if ($db->query($sql_query) === TRUE) {
+         header("location:index.php");
+      } else {
+          echo "Error: " . $sql_query . "<br>" . $db->error;
+      }
+    } elseif(isset($_POST['area'])){
+      $sql_query = "INSERT INTO Geoarea (areaType, areaDescription, vectors ) VALUES ('"
+      . $_POST['type'] . "',' "
+      . $_POST['description'] . "',' "
+      . $_POST['vectors'] . "');";
+
+      if ($db->query($sql_query) === TRUE) {
+         header("location:index.php");
+      } else {
+          echo "Error: " . $sql_query . "<br>" . $db->error;
       }
     }
-	?>
+  }
+  if(isset($_POST['submitUpdate'])) {
+    if(isset($_POST['point'])){
+      $sql_query = "UPDATE GeoPoint SET
+      pointType='".$_POST['type']."',
+      pointDescription='".$_POST['description']."',
+      coordinateX=".$_POST['coordinateX'].",
+      coordinateY=".$_POST['coordinateY']."
+       WHERE pointID=".$_GET['id'];
 
-	<form action="" method="POST">
+      if($db->query($sql_query) === TRUE) {
+        header("location:index.php");
+      } else {
+          echo "Error: " . $sql_query . "<br>" . $db->error;
+      }
+    } elseif(isset($_POST['path'])){
+      $sql_query = "UPDATE Geopath SET
+      pathType='".$_POST['type']."',
+      pathDescription='".$_POST['description']."',
+      vectors='".$_POST['vectors']."'
+       WHERE pathID=".$_GET['id'];
+
+      if($db->query($sql_query) === TRUE) {
+        header("location:index.php");
+      } else {
+          echo "Error: " . $sql_query . "<br>" . $db->error;
+      }
+    }elseif(isset($_POST['area'])){
+      $sql_query = "UPDATE Geoarea SET
+      areaType='".$_POST['type']."',
+      areaDescription='".$_POST['description']."',
+      vectors='".$_POST['vectors']."'
+       WHERE areaID=".$_GET['id'];
+
+      if($db->query($sql_query) === TRUE) {
+        header("location:index.php");
+      } else {
+          echo "Error: " . $sql_query . "<br>" . $db->error;
+      }
+    }
+  }
+?>
+
+<form action="" method="POST">
+  <div class='editContent'>
 		Name of the element <br>
     <input type="text" name="type" value=<?php echo "\"" . $type . "\"";?>><br>
 		Description : (accepts html)<br>
@@ -203,14 +206,14 @@ File handling the creation and edition of points, area and path
 
     //changes if we are updating or adding a new value
     if(isset($_GET['id'])) {
-      echo "<input type='submit' name='submitUpdate' value='Update element'>";
+      echo "<input class='backButton' type='submit' name='submitUpdate' value='Update element'>";
     } else {
-      echo "<input type='submit' name='submitAdd' value='Add element'>";
+      echo "<input class='backButton' type='submit' name='submitAdd' value='Add element'>";
     }
     ?>
-    <a href='./'>Back to the map</a>
-	</form>
-</body>
+    <a class='backButton' href='./'>Back to the map</a>
+  </div>
+</form>
 
 <?
 include('../../inc/footer.inc');

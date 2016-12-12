249047d1cea0f504f39ed969ac483681f1a97aac
Alexa Noel <alexanowel@live.fr>
2016-12-12 19:33:55 +0000

    delete security

--- app/pages/clubs/delete.php
+++ app/pages/clubs/delete.php
@@ -2,6 +2,24 @@
 
 include("../../dbconnect.php");
 
+echo "	<section>
+  <div class='edition'>";
+$sql_query="SELECT clubID from junctionuserclub
+where username='" . $_SESSION['username'] ."'";
+$result = $db->query($sql_query);
+$clubs = array();
+while ($row = $result->fetch_array()) {
+  array_push($clubs, $row['clubID']);
+}
+
+if(!(isset($_SESSION['userType']))) {
+  header('location:./');
+} else if($_SESSION['userType'] != "admin"
+&& $_SESSION['userType'] != "clubAdmin") {
+  header('location:./');
+} else if($_SESSION['userType'] == "clubAdmin" && (!(in_array($_GET['clubID'], $clubs)))) {
+  header('location:./');
+}
 
 $sql_query="delete from clubeventassociation
 where clubID=" . $_GET['clubID'];
--- app/pages/health/delete.php
+++ app/pages/health/delete.php
@@ -2,6 +2,13 @@
 
 include('../../dbconnect.php');
 global $db;
+
+if(!(isset($_SESSION['userType']))) {
+  header('location:./');
+} else if($_SESSION['userType'] !== 'admin') {
+    header('location:./');
+}
+
 $sql_query="delete from HWNews
 where HWNewsID=" . $_GET['HWNewsID'];
 if($db->query($sql_query) === TRUE) {
